19dc5ba25d1f468c15afabc76307138cd51ebbc5
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-09-01 15:45:30 +0600

    the

--- index.js
+++ index.js
@@ -69,6 +69,160 @@ async function run() {
       const result = await userCollection.insertOne(user);
       return res.send(result);
     });
+    //payment Route
+    // ToDo: have to receive data from front-end
+    app.post("/order", async (req, res) => {
+      const mail = "muhammadformaanali@gmail.com";
+      console.log(mail);
+
+      const result = await cartsCollection.find({ userEmail: mail }).toArray();
+
+      const orderedBooks = result.map((book) => {
+        const { bookId, title, author, image_url } = book;
+        const orderedItem = {
+          title,
+          bookId,
+          author,
+          image_url,
+        };
+        return orderedItem;
+      });
+      // console.log(orderedBooks);
+      const tran_id = new ObjectId().toString();
+
+      const data = {
+        total_amount: 100,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/failed/${tran_id}`,
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: "Customer Name",
+        cus_email: "customer@example.com",
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+
+      // console.log(data)
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalOrder = {
+          mail,
+          orderedBooks,
+          paidStatus: "unpaid",
+          transactionId: tran_id,
+        };
+
+        const result = ordersCollection.insertOne(finalOrder);
+      });
+
+      app.post("/payment/success/:tranId", async (req, res) => {
+        const result = await ordersCollection.updateOne(
+          { transactionId: req.params.tranId },
+          {
+            $set: {
+              paidStatus: "paid",
+            },
+          }
+        );
+        if (result.modifiedCount > 0) {
+          const deleteCart = await cartsCollection.deleteMany({
+            userEmail: mail,
+          });
+          res.redirect("http://localhost:3000/dashboard/cart/payment-success");
+        }
+      });
+
+      app.post("/payment/failed/:tranId", async (req, res) => {
+        // console.log(req.params.tranId)
+        const result = await ordersCollection.deleteOne({
+          transactionId: req.params.tranId,
+        });
+        if (result.deletedCount > 0) {
+          res.redirect("http://localhost:3000/dashboard/cart/payment-failed");
+        }
+      });
+    });
+
+    // route for get all users data
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Route for update user role
+    app.put("/users/update-role/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const { updatedRole } = req.body;
+      const query = { _id: new ObjectId(userId) };
+      const option = { upsert: true };
+      const updateOperation = {
+        $set: { role: updatedRole },
+      };
+      const updateResult = await usersCollection.updateOne(
+        query,
+        updateOperation,
+        option
+      );
+      res.send(updateResult);
+    });
+
+    // Route for update status
+    app.put("/users/update-status/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const { updatedStatus } = req.body;
+      const query = { _id: new ObjectId(userId) };
+      const option = { upsert: true };
+      const updateOperation = {
+        $set: { status: updatedStatus },
+      };
+      const updateResult = await usersCollection.updateOne(
+        query,
+        updateOperation,
+        option
+      );
+      res.send(updateResult);
+    });
+
+    // route for warning
+    app.put("/users/warning/:userId", async (req, res) => {
+      const { userId } = req.params;
+      const { warning } = req.body;
+      const query = { _id: new ObjectId(userId) };
+      const option = { upsert: true };
+      const updateOperation = {
+        $set: { warning: warning },
+      };
+      const updateResult = await usersCollection.updateOne(
+        query,
+        updateOperation,
+        option
+      );
+      res.send(updateResult);
+    });
     // book api
     app.get("/books", async (req, res) => {
       const books = await bookCollection.find().toArray();
@@ -93,166 +247,6 @@ async function run() {
         const uniqueCategories = [
           ...new Set(result.map((book) => book.category)),
         ];
-        //payment Route
-        // ToDo: have to receive data from front-end
-        app.post("/order", async (req, res) => {
-          const mail = "muhammadformaanali@gmail.com";
-          console.log(mail);
-
-          const result = await cartsCollection
-            .find({ userEmail: mail })
-            .toArray();
-
-          const orderedBooks = result.map((book) => {
-            const { bookId, title, author, image_url } = book;
-            const orderedItem = {
-              title,
-              bookId,
-              author,
-              image_url,
-            };
-            return orderedItem;
-          });
-          // console.log(orderedBooks);
-          const tran_id = new ObjectId().toString();
-
-          const data = {
-            total_amount: 100,
-            currency: "BDT",
-            tran_id: tran_id, // use unique tran_id for each api call
-            success_url: `http://localhost:5000/payment/success/${tran_id}`,
-            fail_url: `http://localhost:5000/payment/failed/${tran_id}`,
-            cancel_url: "http://localhost:3030/cancel",
-            ipn_url: "http://localhost:3030/ipn",
-            shipping_method: "Courier",
-            product_name: "Computer.",
-            product_category: "Electronic",
-            product_profile: "general",
-            cus_name: "Customer Name",
-            cus_email: "customer@example.com",
-            cus_add1: "Dhaka",
-            cus_add2: "Dhaka",
-            cus_city: "Dhaka",
-            cus_state: "Dhaka",
-            cus_postcode: "1000",
-            cus_country: "Bangladesh",
-            cus_phone: "01711111111",
-            cus_fax: "01711111111",
-            ship_name: "Customer Name",
-            ship_add1: "Dhaka",
-            ship_add2: "Dhaka",
-            ship_city: "Dhaka",
-            ship_state: "Dhaka",
-            ship_postcode: 1000,
-            ship_country: "Bangladesh",
-          };
-
-          // console.log(data)
-
-          const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
-          sslcz.init(data).then((apiResponse) => {
-            // Redirect the user to payment gateway
-            let GatewayPageURL = apiResponse.GatewayPageURL;
-            res.send({ url: GatewayPageURL });
-
-            const finalOrder = {
-              mail,
-              orderedBooks,
-              paidStatus: "unpaid",
-              transactionId: tran_id,
-            };
-
-            const result = ordersCollection.insertOne(finalOrder);
-          });
-
-          app.post("/payment/success/:tranId", async (req, res) => {
-            const result = await ordersCollection.updateOne(
-              { transactionId: req.params.tranId },
-              {
-                $set: {
-                  paidStatus: "paid",
-                },
-              }
-            );
-            if (result.modifiedCount > 0) {
-              const deleteCart = await cartsCollection.deleteMany({
-                userEmail: mail,
-              });
-              res.redirect(
-                "http://localhost:3000/dashboard/cart/payment-success"
-              );
-            }
-          });
-
-          app.post("/payment/failed/:tranId", async (req, res) => {
-            // console.log(req.params.tranId)
-            const result = await ordersCollection.deleteOne({
-              transactionId: req.params.tranId,
-            });
-            if (result.deletedCount > 0) {
-              res.redirect(
-                "http://localhost:3000/dashboard/cart/payment-failed"
-              );
-            }
-          });
-        });
-
-        // route for get all users data
-        app.get("/users", async (req, res) => {
-          const result = await usersCollection.find().toArray();
-          res.send(result);
-        });
-
-        // Route for update user role
-        app.put("/users/update-role/:userId", async (req, res) => {
-          const { userId } = req.params;
-          const { updatedRole } = req.body;
-          const query = { _id: new ObjectId(userId) };
-          const option = { upsert: true };
-          const updateOperation = {
-            $set: { role: updatedRole },
-          };
-          const updateResult = await usersCollection.updateOne(
-            query,
-            updateOperation,
-            option
-          );
-          res.send(updateResult);
-        });
-
-        // Route for update status
-        app.put("/users/update-status/:userId", async (req, res) => {
-          const { userId } = req.params;
-          const { updatedStatus } = req.body;
-          const query = { _id: new ObjectId(userId) };
-          const option = { upsert: true };
-          const updateOperation = {
-            $set: { status: updatedStatus },
-          };
-          const updateResult = await usersCollection.updateOne(
-            query,
-            updateOperation,
-            option
-          );
-          res.send(updateResult);
-        });
-
-        // route for warning
-        app.put("/users/warning/:userId", async (req, res) => {
-          const { userId } = req.params;
-          const { warning } = req.body;
-          const query = { _id: new ObjectId(userId) };
-          const option = { upsert: true };
-          const updateOperation = {
-            $set: { warning: warning },
-          };
-          const updateResult = await usersCollection.updateOne(
-            query,
-            updateOperation,
-            option
-          );
-          res.send(updateResult);
-        });
 
         const categoryCounts = uniqueCategories.map((category) => {
           const count = result.filter(
@@ -402,7 +396,7 @@ async function run() {
     });
     app.patch("/book-shelf", async (req, res) => {
       const id = req.query.id;
-      const shelfName = req.body; 
+      const shelfName = req.body;
       const filter = { _id: new ObjectId(id) };
       const option = { upsert: false };
       const updateOperation = {
