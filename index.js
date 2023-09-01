76ee9ac0eaa1630d282ca39a9da613f411e6f730
unknown <MuhammadFormanAli>
2023-09-01 16:23:24 +0600

    asdfgh

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const SSLCommerzPayment = require("sslcommerz-lts");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Set the environment variable to disable saslprep warnings
-process.env.MONGOMS_DISABLE_SASLPREP = "1";
+// Set the environment variable to specify the saslprep library
+process.env.SASL_PREP_LIB = require.resolve("saslprep");
 
 // middleware
 app.use(cors());
@@ -36,29 +36,47 @@ const is_live = false; //true for live, false for sandbox
 async function run() {
   try {
     const bookCollection = client.db("i-library").collection("books");
+    const userCollection = client.db("i-library").collection("users");
+    const requestedBooks = client.db("i-library").collection("requested-books");
     const cartsCollection = client.db("i-library").collection("carts");
     const wishListCollection = client.db("i-library").collection("wishList");
+    const donatedBooks = client.db("i-library").collection("donated-books");
+    const bookShelfCollection = client.db("i-library").collection("bookShelf");
+    const allBlogsCollection = client
+      .db("i-library")
+      .collection("allBlogsCollection");
+    const reviewCollection = client.db("i-library").collection("reviews");
+    const QACollection = client.db("i-library").collection("qa");
     const ordersCollection = client.db("i-library").collection("orders");
     const usersCollection = client.db("i-library").collection("users");
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    app.get("/books", async (req, res) => {
-      const books = await bookCollection.find().toArray();
-      res.send(books);
-    });
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
 
+    // user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      return res.send(result);
+    });
     //payment Route
     // ToDo: have to receive data from front-end
     app.post("/order", async (req, res) => {
-      console.log(req.body);
+      // console.log(req.body);
+    
 
       const {name, email, borrowDate,duration,returnDate,shippingAddress} = req.body
       // console.log(email)
-
-
-      name, email, borrowDate,duration,returnDate,shippingAddress
 
       const result = await cartsCollection.find({ userEmail: email }).toArray();
       console.log(result);
@@ -158,20 +176,16 @@ async function run() {
       });
     });
 
-
     // route for get all users data
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
-
-
     // Route for update user role
     app.put("/users/update-role/:userId", async (req, res) => {
       const { userId } = req.params;
       const { updatedRole } = req.body;
-      console.log(userId, updatedRole);
       const query = { _id: new ObjectId(userId) };
       const option = { upsert: true };
       const updateOperation = {
@@ -185,12 +199,10 @@ async function run() {
       res.send(updateResult);
     });
 
-
-    // Route for update status 
+    // Route for update status
     app.put("/users/update-status/:userId", async (req, res) => {
       const { userId } = req.params;
       const { updatedStatus } = req.body;
-      console.log(userId, updatedStatus);
       const query = { _id: new ObjectId(userId) };
       const option = { upsert: true };
       const updateOperation = {
@@ -204,12 +216,10 @@ async function run() {
       res.send(updateResult);
     });
 
-
-    // route for warning 
+    // route for warning
     app.put("/users/warning/:userId", async (req, res) => {
       const { userId } = req.params;
       const { warning } = req.body;
-      console.log(userId, );
       const query = { _id: new ObjectId(userId) };
       const option = { upsert: true };
       const updateOperation = {
@@ -222,9 +232,223 @@ async function run() {
       );
       res.send(updateResult);
     });
+    // book api
+    app.get("/books", async (req, res) => {
+      const books = await bookCollection.find().toArray();
+      res.send(books);
+    });
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const books = await bookCollection.findOne(query);
+      res.send(books);
+    });
+    app.get("/books/category", async (req, res) => {
+      try {
+        const projection = {
+          category: 1,
+        };
+        const result = await bookCollection
+          .find()
+          .project(projection)
+          .toArray();
 
+        const uniqueCategories = [
+          ...new Set(result.map((book) => book.category)),
+        ];
 
+        const categoryCounts = uniqueCategories.map((category) => {
+          const count = result.filter(
+            (book) => book.category === category
+          ).length;
+          return {
+            category,
+            count,
+          };
+        });
 
+        res.send(categoryCounts);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred.");
+      }
+    });
+    app.get("/books/category-filter", async (req, res) => {
+      const categoryNames = req.query.categories.split(",");
+      const query = { category: { $in: categoryNames } };
+
+      try {
+        const books = await bookCollection.find(query).toArray();
+        res.send(books);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred.");
+      }
+    });
+    app.get("/books/author", async (req, res) => {
+      try {
+        const projection = {
+          author: 1,
+        };
+        const result = await bookCollection
+          .find()
+          .project(projection)
+          .toArray();
+
+        const uniqueAuthor = [...new Set(result.map((book) => book.author))];
+
+        const authorCounts = uniqueAuthor.map((author) => {
+          const count = result.filter((book) => book.author === author).length;
+          return {
+            author,
+            count,
+          };
+        });
+
+        res.send(authorCounts);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred.");
+      }
+    });
+
+    app.get("/books/author-filter", async (req, res) => {
+      const authorNames = req.query.authors.split(",");
+      const query = { author: { $in: authorNames } };
+
+      try {
+        const books = await bookCollection.find(query).toArray();
+        res.send(books);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred.");
+      }
+    });
+    app.get("/books/publisher", async (req, res) => {
+      try {
+        const projection = {
+          publisher: 1,
+        };
+        const result = await bookCollection
+          .find()
+          .project(projection)
+          .toArray();
+
+        const uniquePublisher = [
+          ...new Set(result.map((book) => book.publisher)),
+        ];
+
+        const publisherCounts = uniquePublisher.map((publisher) => {
+          const count = result.filter(
+            (book) => book.publisher === publisher
+          ).length;
+          return {
+            publisher,
+            count,
+          };
+        });
+
+        res.send(publisherCounts);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred.");
+      }
+    });
+    // Book shelf API
+    app.get("/book-shelf", async (req, res) => {
+      const email = req.query.email;
+      const result = await bookShelfCollection.find({ email: email }).toArray();
+      res.send(result);
+    });
+
+    app.get("/book-shelf/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await bookShelfCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.post("/book-shelf", async (req, res) => {
+      const book = req.body;
+      const newBook = {
+        bookId: book.bookId,
+        bookTitle: book.bookTitle,
+        bookImg: book.bookImg,
+        authorName: book.authorName,
+      };
+      const newShelf = {
+        shelfName: book.shelfName,
+        email: book.userEmail,
+        userName: book.c,
+        books: [newBook],
+      };
+      const filter = { shelfName: book.shelfName, email: book.userEmail };
+
+      try {
+        const existingShelf = await bookShelfCollection.findOne(filter);
+        if (existingShelf) {
+          const result = await bookShelfCollection.updateOne(filter, {
+            $push: { books: newBook },
+          });
+          res.json(result);
+        } else {
+          const result = await bookShelfCollection.insertOne(newShelf);
+          res.json(result);
+        }
+      } catch (error) {
+        console.error("Error adding book to shelf:", error);
+        res.status(500).json({
+          error: "An error occurred while adding the book to the shelf",
+        });
+      }
+    });
+    app.patch("/book-shelf", async (req, res) => {
+      const id = req.query.id;
+      const shelfName = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: false };
+      const updateOperation = {
+        $set: { shelfName: updatedRole },
+      };
+    });
+    app.delete("/book-shelf", async (req, res) => {
+      const id = req.query.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookShelfCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // Book shelf API
+
+    // requested books
+    app.get("/requested-books", async (req, res) => {
+      const email = req.query.email;
+      const filter = {
+        userEmail: email,
+      };
+      const result = await requestedBooks.find(filter).toArray();
+      res.send(result);
+    });
+    app.post("/requested-books", async (req, res) => {
+      const book = req.body;
+      const result = await requestedBooks.insertOne(book);
+      res.send(result);
+    });
+    // doanted books
+    app.get("/donated-books", async (req, res) => {
+      const email = req.query.email;
+      const filter = {
+        userEmail: email,
+      };
+      const result = await donatedBooks.find(filter).toArray();
+      res.send(result);
+    });
+    app.post("/donated-books", async (req, res) => {
+      const book = req.body;
+      const result = await donatedBooks.insertOne(book);
+      res.send(result);
+    });
 
     // route for get Popular Books
     app.get("/popular-books", async (req, res) => {
@@ -233,6 +457,8 @@ async function run() {
       const result = await bookCollection.find().sort(sort).limit(12).toArray();
       res.send(result);
     });
+
+    // cart related api
 
     // route for get new Books
     app.get("/new-books", async (req, res) => {
@@ -273,6 +499,8 @@ async function run() {
     });
 
     // wish list related api
+
+    // wish list related api
     //api for add items in wish list
     app.post("/wish-list", async (req, res) => {
       const wishList = req.body;
@@ -300,6 +528,175 @@ async function run() {
       // console.log(wishList)
       const query = { userEmail: wishList.userEmail, bookId: wishList.bookId };
       const result = await wishListCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Ratings & Reviews Related API
+    app.get("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { "book-id": id };
+      const reviews = await reviewCollection.find(filter).toArray();
+      res.send(reviews);
+    });
+
+    app.get("/reviews/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const reviews = await reviewCollection.find(query).toArray();
+      res.send(reviews);
+    });
+
+    app.get("/blogs", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const skip = page * limit;
+      const result1 = await allBlogsCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+      const result2 = await allBlogsCollection.estimatedDocumentCount();
+
+      res.send([result1, result2]);
+    });
+    app.post("/reviews", async (req, res) => {
+      const {
+        username,
+        rating,
+        review,
+        date,
+        bookTitle,
+        bookImg,
+        bookId,
+        email,
+      } = req.body;
+      // console.log(req.body);
+
+      try {
+        // Insert the new review
+        await reviewCollection.insertOne({
+          username,
+          rating,
+          review,
+          date,
+          "book-name": bookTitle,
+          "book-img": bookImg,
+          "book-id": bookId,
+          email,
+        });
+
+        res.status(201).json({ message: "Review submitted successfully!" });
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Failed to submit review." });
+      }
+    });
+
+    // Book Questions & Answers Related API
+    app.get("/qa/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { "book-id": id };
+      const qa = await QACollection.find(filter).toArray();
+      res.send(qa);
+    });
+
+    app.post("/qa", async (req, res) => {
+      const { username, question, date, bookTitle, bookImg, bookId, email } =
+        req.body;
+      // console.log(req.body);
+
+      try {
+        // Insert the new QA
+        await QACollection.insertOne({
+          username,
+          question,
+          answer: "",
+          "answered-by": "",
+          "answered-date": "",
+          date,
+          "book-name": bookTitle,
+          "book-img": bookImg,
+          "book-id": bookId,
+          email,
+        });
+
+        res.status(201).json({ message: "Question submitted successfully!" });
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Failed to submit question." });
+      }
+      res.send(result);
+    });
+
+    //api for DELETE items from wish list
+    app.delete("/wish-list", async (req, res) => {
+      const wishList = req.body;
+      // console.log(wishList)
+      const query = { userEmail: wishList.userEmail, bookId: wishList.bookId };
+      const result = await wishListCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ------------------searchText-----------------------//
+    app.post("/books", async (req, res) => {
+      const Addededbook = req.body;
+      const result = await bookCollection.insertOne(Addededbook);
+      res.send(result);
+    });
+
+    // ------------------searchText-----------------------//
+    const indexKeys = { title: 1 };
+    const indexOptions = { name: "titlesearch" };
+    const result = await bookCollection.createIndex(indexKeys, indexOptions);
+
+    app.get("/books/:text", async (req, res) => {
+      const searchText = req.params.text;
+      const result = await bookCollection
+        .find({
+          $or: [{ title: { $regex: searchText, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    // ------------------Update-----------------------//
+
+    app.patch("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const Clientdata = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatetoydata = {
+        $set: {
+          title: Clientdata.title,
+          author: Clientdata.author,
+          translator: Clientdata.translator || null,
+          publisher: Clientdata.publisher,
+          shelf: parseFloat(Clientdata.shelf),
+          image_url: Clientdata.image_url,
+          edition: Clientdata.edition,
+          published_in: parseFloat(Clientdata.published_in),
+          category: Clientdata.category,
+          number_of_pages: parseFloat(Clientdata.number_of_pages),
+          language: Clientdata.language,
+          country: Clientdata.country,
+          // ratings:Clientdata.ratings,
+          // total_read:Clientdata.total_read,
+          added_date: Clientdata.added_date,
+          hard_copy: Clientdata.hard_copy,
+          pdf: Clientdata.pdf,
+          ebook: Clientdata.ebook,
+          pdf_link: Clientdata.pdf_link,
+        },
+      };
+      const result = await bookCollection.updateOne(filter, updatetoydata);
+      res.send(result);
+    });
+
+    // ------------------Delete-----------------------//
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
       res.send(result);
     });
 
